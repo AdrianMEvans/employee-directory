08758515b1a3382a1f56c165775950c450c71314
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-05-31 22:50:26 +0800

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
-// import SearchBar from "./components/SearchBar";
 import DataContainer from "./components/Container/DataContainer.js";
 
 const App = () => {
   return (
-    <div>
-      {/* <SearchBar /> */}
+    <>
       <DataContainer />
-      </div>
+    </>
   );
 };
 
